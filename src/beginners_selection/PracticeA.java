e17fa169fe08d87fae03bfc808939e6e20b4e432
appleKyamamoto <mtbook201@gmail.com>
2019-05-20 00:04:48 +0900

    Update PracticeA.java
    PracticeAの内容を記述

--- src/beginners_selection/PracticeA.java
+++ src/beginners_selection/PracticeA.java
@@ -1,10 +1,23 @@
 package beginners_selection;
 
-public class PracticeA {
+import java.util.Scanner;
 
+public class PracticeA {
+	/*
+	 * 問題：
+	 * https://atcoder.jp/contests/abs/tasks/practice_1 
+	 */
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		int a,b,c;
+		String s;
+		try(Scanner sc = new Scanner(System.in)){
+		    a = sc.nextInt();
+		    b = sc.nextInt();
+		    c = sc.nextInt();
+		    s = sc.next();
+		}
+	    System.out.println((a+b+c)+" "+s);
 	}
 
 }
